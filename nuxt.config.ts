80d6bfc243c9c9a257ad16a3be2a843be5334304
BUBUSTEIN <bubustein.official@gmail.com>
2024-02-19 12:15:43 +0200

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
                 content: 'width=device-width, initial-scale=1'
             }
         ],
+        link:[{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
         script: [
             {
                 src: 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
